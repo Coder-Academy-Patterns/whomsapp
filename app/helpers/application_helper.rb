488f55a94bb7da85a6abb377a9106362c7a96726
Patrick Smith <patrick@burntcaramel.com>
2017-10-23 16:36:09 +1100

    Add automatic emoji replacement to messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  EMOJI_CODES_TABLE = {
+    ':smiley:' => '😃',
+    ':sleeping:' => '😴',
+    ':joy:' => '😂',
+    ':star:' => '⭐️',
+    ':dog:' => '🐶',
+    ':monkey_face:' => '🐵'
+  }
+
+  def format_emoji_codes(input)
+    # Doesn’t handle when code is not valid
+    # input.gsub(/:[a-z_]+:/, EMOJI_CODES_TABLE)
+    
+    # Does handle when code is not valid
+    input.gsub(/:[a-z_]+:/) do |match|
+      EMOJI_CODES_TABLE.fetch(match, match)
+    end
+  end
 end
